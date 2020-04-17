b6ca43223608df3155c9d125e1d88b042a7c16f0
thsvr <vr.ths.zd@gmail.com>
2020-04-17 13:19:55 -0300

    Add structure of Recipe component.

---
+++ my-app/src/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Recipe = () => {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>Colories</p>
+      <img scr=" " alt="#" />
+    </div>
+  );
+};
+
+export default Recipe;
