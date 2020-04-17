e0c50b4fa92c94dd4b4cd7c9cd1d0ed1d2a68108
thsvr <vr.ths.zd@gmail.com>
2020-04-17 13:16:54 -0300

    Create async/await func to get part of the api.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState}  from 'react';
 import './App.css';
 
 const App = () => {
-
   const  APP_ID = '734e3200';
-  const APP_KEY = 'b1a351177cadeaae51b332ed3221afaf'
+  const APP_KEY = 'b1a351177cadeaae51b332ed3221afaf';
 
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([]);
+  
+  useEffect( () =>{
+    getRecipes();
+  }, []);
 
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
