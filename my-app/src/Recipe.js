fbfa7b8576e2c4c18c006c61d4cb96e4dcf4f1c1
thsvr <vr.ths.zd@gmail.com>
2020-04-17 16:40:28 -0300

    Add a module to style Recipe component.

--- my-app/src/Recipe.js
+++ my-app/src/Recipe.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './recipe.module.css';
 
 const Recipe = ({title, calories, image, ingredients}) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>      
-      <img src={image} alt="#"/>
+      <img className={style.image} src={image} alt="#"/>
       <ol>
         {ingredients.map(ingredients =>(
           <li>{ingredients.text}</li>
