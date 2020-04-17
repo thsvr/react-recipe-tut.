83c7ae9cd3658c8741f5a0c0f6b0d2a99d41e38e
thsvr <vr.ths.zd@gmail.com>
2020-04-17 14:57:57 -0300

    Add updateSearch().

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,8 @@ const App = () => {
   const APP_KEY = 'b1a351177cadeaae51b332ed3221afaf';
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
   
   useEffect( () =>{
     getRecipes();
@@ -14,24 +16,33 @@ const App = () => {
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
       );
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   };
 
+  const updateSearch = e => {
+    setSearch(e.target.value)
+    console.log(search)
+  };
 
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text"></input>
+        <input 
+        className="search-bar" 
+        type="text" 
+        value={search}
+        onChange={updateSearch}></input>
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
       {recipes.map(recipe => (
         <Recipe 
+        key={recipe.recipe.label}
         title={recipe.recipe.label}
         calories={recipe.recipe.calories}
         image={recipe.recipe.image}
